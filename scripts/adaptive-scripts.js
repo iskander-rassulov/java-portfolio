c82077777daa2a80637d82b6dce886a430702b94
iskander-rassulov <iskander.rassulov.2002@gmail.com>
2025-02-20 21:55:38 +0500

    redesign v0.01

--- scripts/adaptive-scripts.js
+++
@@ -1,9 +0,0 @@
-function adjustRootFontSize() {
-    let width = window.innerWidth;
-    let fontSize = Math.max(12, Math.min(16, width / 120)); // Границы от 12px до 16px
-    
-    document.documentElement.style.fontSize = fontSize + 'px';
-}
-
-window.addEventListener("resize", adjustRootFontSize);
-adjustRootFontSize(); // Запуск при загрузке страницы
