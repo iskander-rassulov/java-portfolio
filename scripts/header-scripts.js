1dc962b848ede2f95cb40f73ee9d20478bf50723
iskander-rassulov <iskander.rassulov.2002@gmail.com>
2025-02-26 17:56:05 +0500

    header

--- scripts/header-scripts.js
+++ scripts/header-scripts.js
@@ -1,23 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const navLinks = document.querySelectorAll('header nav ul li a');
-  const sections = document.querySelectorAll('section');
+document.addEventListener('DOMContentLoaded', function () {
+  const links = document.querySelectorAll('.nav-link');
+  const currentPath = window.location.pathname;
 
-  // Обработчик кликов по навигации
-  navLinks.forEach(link => {
-    link.addEventListener('click', event => {
-      event.preventDefault(); // Отключаем переход по якорям
-      const targetId = link.getAttribute('href').substring(1); // Получаем ID секции
-      const targetSection = document.getElementById(targetId);
-
-      // Скрываем все секции
-      sections.forEach(section => {
-        section.classList.remove('fade-in');
-        section.classList.add('fade-out');
-      });
-
-      // Показываем целевую секцию
-      targetSection.classList.remove('fade-out');
-      targetSection.classList.add('fade-in');
-    });
+  links.forEach(link => {
+      if (link.getAttribute('href') === currentPath) {
+          link.classList.add('active');
+      }
   });
 });
+
